82a080bb826e40a7e070a43e48d7604cfbb40c40
justinburgess <signalincode@gmail.com>
2022-06-06 20:56:04 -0400

    change menu icons svg to img

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const newsElement = document.getElementById('menu-news');
+const styleElement = document.getElementById('menu-style');
+const galleryElement = document.getElementById('menu-gallery');
+const gameElement = document.getElementById('menu-game');
+const dashboardElement = document.getElementById('menu-dashboard')
+const directoryElement = document.getElementById('menu-directory');
+const iconsElement =document.getElementById('icons')
+
+icons.addEventListener('click', (e)=>{
+    const target = e.target;
+    console.log(target);
+}); 
